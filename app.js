4b298eadc7444fee18ecf6964653362765beeb53
anderosh <anderosh@gmail.com>
2020-02-04 02:55:58 -0500

    CORS setup

--- app.js
+++ app.js
@@ -7,6 +7,21 @@ const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 3001
 const connectionString = process.env.DB_CONECTION
 
+// C.O.R.S
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  // res.header(
+  //   'Access-Control-Allow-Headers',
+  //   'Origin, X-Requested-With, Content-Type, Accept'
+  // );
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  )
+  next()
+})
+
 // MIDLEWARES
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
